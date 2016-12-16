90295e6d2a530f96e3af6a63c81a4ea54c48df9f
Mark Williams <oi.markwilliams@gmail.com>
2016-12-16 11:02:21 +0000

    Optional day forecast

--- README.md
+++ README.md
@@ -8,6 +8,12 @@
 
 !['Screenshot of python script output'](cloudbase.jpg)
 
+# Future Cloud Base Forecast
+To get cloud base forecasts for future dates, add an optional day argument to the
+script. Day indexes are zero-based
+e.g. `python cloudbase.py myapikeymyapikeymyapikey 52.518342 -2.871176 1` will return
+the cloud base forecast for tomorrow.
+
 ##Thanks to
 [Christopher Blunck](http://pydoc.net/Python/weather/0.9.1/weather.units.temp/) and (Meteo Blog)[http://www.meteo-blog.net/2012-05/dewpoint-calculation-script-in-python/]
 for the temperature conversion and dewpoint estimation functions I've used in this script.
--- cloudbase.py
+++ cloudbase.py
@@ -33,15 +33,17 @@ if __name__ == "__main__":
         sys.exit()
     lat = sys.argv[2] if len(sys.argv) > 2 else "52.5189846"
     lng = sys.argv[3] if len(sys.argv) > 3 else "-2.8909141"
+    day = int(sys.argv[4]) if len(sys.argv) > 4 else 0
 
     url = "https://api.darksky.net/forecast/{0}/{1},{2}?exclude=minutely,hourly,flags".format(api_key, lat, lng)
     response = urllib.urlopen(url)
     data = json.loads(response.read())
-    min_temp = weatherunitstemp.fahrenheit_to_celsius(data["daily"]["data"][0]["temperatureMin"])
+    day_data = data["daily"]["data"][day]
+    min_temp = weatherunitstemp.fahrenheit_to_celsius(day_data["temperatureMin"])
     print "Min forecast temperature for the day is {0}C".format(min_temp)
-    max_temp = weatherunitstemp.fahrenheit_to_celsius(data["daily"]["data"][0]["temperatureMax"])
+    max_temp = weatherunitstemp.fahrenheit_to_celsius(day_data["temperatureMax"])
     print "Max forecast temperature for the day is {0}C".format(min_temp)
-    humidity = data["daily"]["data"][0]["humidity"] * 100
+    humidity = day_data["humidity"] * 100
     print "Humidity is {0}%".format(humidity)
     dewpoint_temp = dewpoint_approximation(min_temp, humidity)
     print "Dewpoint temperature is {0} Celsius".format(dewpoint_temp)
