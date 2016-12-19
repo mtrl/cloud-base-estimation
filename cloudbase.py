929ac2e3f78cb2966bb706e86b4445b0a7457a5c
Mark Williams <oi.markwilliams@gmail.com>
2016-12-19 09:58:10 +0000

    Added hourly forecasts

--- cloudbase.py
+++ cloudbase.py
@@ -27,16 +27,8 @@ def calc_cloudbase_height(dewpoint, max_day_temp):
     '''
     return ((max_day_temp - dewpoint) / 2.5) * 1000
 
-if __name__ == "__main__":
-    # Get data from API
-    if len(sys.argv) > 1:
-        api_key = sys.argv[1]
-    else:
-        sys.exit()
-    lat = sys.argv[2] if len(sys.argv) > 2 else "52.5189846"
-    lng = sys.argv[3] if len(sys.argv) > 3 else "-2.8909141"
-    day = int(sys.argv[4]) if len(sys.argv) > 4 else 0
-
+def day_forecast(lat, lng, day):
+    day = int(day)
     url = "https://api.darksky.net/forecast/{0}/{1},{2}?exclude=minutely,hourly,flags".format(api_key, lat, lng)
     response = urllib.urlopen(url)
     data = json.loads(response.read())
@@ -60,3 +52,37 @@ if __name__ == "__main__":
     print " Dew point temperature:       {:.1f}°C".format(dew_point)
     print " Humidity:                    {:.0f}%".format(humidity)
     print "+-----------------------------------------+"
+
+def hourly_forecast(lat, lng):
+    url = "https://api.darksky.net/forecast/{0}/{1},{2}?exclude=minutely,daily,flags,events".format(api_key, lat, lng)
+    response = urllib.urlopen(url)
+    data = json.loads(response.read())
+    hourly_data = data["hourly"]["data"]
+
+    print "+-----------------------------------------+"
+    print "| Hourly cloud base forecast              |"
+    print "+-----------------------------------------+"
+
+    for hour in hourly_data:
+        timestamp = hour["time"]
+        humidity = hour["humidity"] * 100
+        dew_point = weatherunitstemp.fahrenheit_to_celsius(hour["dewPoint"])
+        temp = weatherunitstemp.fahrenheit_to_celsius(hour["temperature"])
+
+        utc_time = datetime.fromtimestamp(timestamp)
+        cloud_base = calc_cloudbase_height(dew_point, temp)
+
+        print "{0}  {1} ft AGL".format(utc_time.strftime("%a @ %H:%M"), int(cloud_base))
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        api_key = sys.argv[1]
+    else:
+        sys.exit()
+    lat = sys.argv[2] if len(sys.argv) > 2 else "52.5189846"
+    lng = sys.argv[3] if len(sys.argv) > 3 else "-2.8909141"
+    day = sys.argv[4] if len(sys.argv) > 4 else 0
+    if day == "hourly":
+        hourly_forecast(lat=lat, lng=lng)
+    else:
+        day_forecast(lat=lat, lng=lng, day=day)
